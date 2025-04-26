0456e03fdc6e2f9f9c6e49a47678e842c9383b06
RiadElamine <RiadElamineDevInfo@gmail.com>
2025-04-26 13:08:06 +0100

    Update IdentifyRealType.cpp

--- ex02/IdentifyRealType.cpp
+++ ex02/IdentifyRealType.cpp
@@ -44,21 +44,21 @@ void	identify(Base& p)
 {
     try
     {
-        dynamic_cast<A&>(p);
+        (void)dynamic_cast<A&>(p);
         std::cout << "A" << std::endl;
     }
     catch(const std::exception& e)
     {
         try
         {
-            dynamic_cast<B&>(p);
+            (void)dynamic_cast<B&>(p);
             std::cout << "B" << std::endl;
         }
         catch(const std::exception& e)
         {
             try
             {
-                dynamic_cast<C&>(p);
+                (void)dynamic_cast<C&>(p);
                 std::cout << "C" << std::endl;
             }
             catch(const std::exception& e)
